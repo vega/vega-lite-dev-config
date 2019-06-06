cce2366a0b7fbbcf52f5d41acecb156ad07a6758
Kanit Wongsuphasawat <kanitw@gmail.com>
2019-06-06 15:57:35 -0700

    fix: Don't test js file by default + bump to 0.1.6

--- configs/jest.js
+++ configs/jest.js
@@ -3,7 +3,7 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.jsx?$': 'babel-jest'
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/', '<rootDir>/build/'],
   coverageDirectory: './coverage/',
