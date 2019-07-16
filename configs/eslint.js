32c5f9eb2c465271f4ef9f9c25cbd2eab0f5c404
Dominik Moritz <domoritz@gmail.com>
2019-07-16 11:00:02 +0200

    Ignore the configs by default

--- configs/eslint.js
+++ configs/eslint.js
@@ -15,7 +15,7 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    // project: "tsconfig.json", // makes things slow, see https://github.com/typescript-eslint/typescript-eslint/issues/389
+    project: "tsconfig.json",
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module' // Allows for the use of imports
   },
@@ -26,7 +26,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/prefer-for-of': 'error',
-    // "@typescript-eslint/no-for-in-array": "error", // requires type information
+    "@typescript-eslint/no-for-in-array": "error", // requires type information
     '@typescript-eslint/no-require-imports': 'error',
     '@typescript-eslint/no-parameter-properties': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
@@ -54,5 +54,6 @@ module.exports = {
     'no-console': 'off', // we use console
     'no-undef': 'off', // typescript takes care of this for us
     'no-unreachable': 'off' // typescript takes care of this for us
-  }
+  },
+  ignore: ['.eslintrc.js', 'prettier.config.js']
 };
